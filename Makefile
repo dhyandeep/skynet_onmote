81e7688e74f479ae0fe30ccba7890a29d7fef5ee
Dhyan Deep AK <dhyandeep7@gmail.com>
2016-03-13 17:17:46 +0530

    Memory usage optimised

--- Makefile
+++ Makefile
@@ -2,5 +2,7 @@ CONTIKI_PROJECT = dctincontiki
 all: $(CONTIKI_PROJECT)
 
 TARGET_LIBFILES += -lm
+CONTIKI_NO_NET=1
+SMALL=1
 CONTIKI = /home/dhyandeepak/Desktop/Project/Tools/contiki-3.0
 include $(CONTIKI)/Makefile.include
--- dctincontiki.c
+++ dctincontiki.c
@@ -38,115 +38,22 @@
  */
 
 #include "contiki.h"
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h> /* For printf() */
 #include <cfs/cfs.h>
-#include <cfs-coffee.h>
 #include <math.h>
 
 #define N 8
-#define LENGTH 128
-char* INFILE1 ="/home/dhyandeepak/Desktop/input1.bmp";
-char* INFILE2 ="/home/dhyandeepak/Desktop/input2.bmp";
+#define LENGTH 64
+#define PROCESS_CONF_NO_PROCESS_NAMES 1
+typedef unsigned char u8_t; 
+char* INFILE1 ="/home/dhyandeepak/Desktop/outa.bmp";
+char* INFILE2 ="/home/dhyandeepak/Desktop/outb.bmp";
 char* OUTFILE ="/home/dhyandeepak/Desktop/out.pgm";
 
-int mat[128][128];
+u8_t mat[LENGTH][LENGTH];
 unsigned char info1[56];
-static inline void fdct_1d(float *dst, const float *src,
-                           int dst_stridea, int dst_strideb,
-                           int src_stridea, int src_strideb)
-{
-    int i;
 
-    for (i = 0; i < N; i++) {
-        const float x00 = src[0*src_stridea] + src[7*src_stridea];
-        const float x01 = src[1*src_stridea] + src[6*src_stridea];
-        const float x02 = src[2*src_stridea] + src[5*src_stridea];
-        const float x03 = src[3*src_stridea] + src[4*src_stridea];
-        const float x04 = src[0*src_stridea] - src[7*src_stridea];
-        const float x05 = src[1*src_stridea] - src[6*src_stridea];
-        const float x06 = src[2*src_stridea] - src[5*src_stridea];
-        const float x07 = src[3*src_stridea] - src[4*src_stridea];
-        const float x08 = x00 + x03;
-        const float x09 = x01 + x02;
-        const float x0a = x00 - x03;
-        const float x0b = x01 - x02;
-        const float x0c = 1.38703984532215*x04 + 0.275899379282943*x07;
-        const float x0d = 1.17587560241936*x05 + 0.785694958387102*x06;
-        const float x0e = -0.785694958387102*x05 + 1.17587560241936*x06;
-        const float x0f = 0.275899379282943*x04 - 1.38703984532215*x07;
-        const float x10 = 0.353553390593274 * (x0c - x0d);
-        const float x11 = 0.353553390593274 * (x0e - x0f);
-        dst[0*dst_stridea] = 0.353553390593274 * (x08 + x09);
-        dst[1*dst_stridea] = 0.353553390593274 * (x0c + x0d);
-        dst[2*dst_stridea] = 0.461939766255643*x0a + 0.191341716182545*x0b;
-        dst[3*dst_stridea] = 0.707106781186547 * (x10 - x11);
-        dst[4*dst_stridea] = 0.353553390593274 * (x08 - x09);
-        dst[5*dst_stridea] = 0.707106781186547 * (x10 + x11);
-        dst[6*dst_stridea] = 0.191341716182545*x0a - 0.461939766255643*x0b;
-        dst[7*dst_stridea] = 0.353553390593274 * (x0e + x0f);
-        dst += dst_strideb;
-        src += src_strideb;
-    }
-}
 
-static void fdct(float *dst, const float *src)
-{
-    float tmp[N*N];
-    fdct_1d(tmp, src, 1, N, 1, N);
-    fdct_1d(dst, tmp, N, 1, N, 1);
-}
-
-static inline void idct_1d(float *dst, const float *src,
-                           int dst_stridea, int dst_strideb,
-                           int src_stridea, int src_strideb)
-{
-    int i;
-
-    for (i = 0; i < N; i++) {
-        const float x00 = 1.4142135623731*src[0*src_stridea];
-        const float x01 = 1.38703984532215*src[1*src_stridea] + 0.275899379282943*src[7*src_stridea];
-        const float x02 = 1.30656296487638*src[2*src_stridea] + 0.541196100146197*src[6*src_stridea];
-        const float x03 = 1.17587560241936*src[3*src_stridea] + 0.785694958387102*src[5*src_stridea];
-        const float x04 = 1.4142135623731*src[4*src_stridea];
-        const float x05 = -0.785694958387102*src[3*src_stridea] + 1.17587560241936*src[5*src_stridea];
-        const float x06 = 0.541196100146197*src[2*src_stridea] - 1.30656296487638*src[6*src_stridea];
-        const float x07 = -0.275899379282943*src[1*src_stridea] + 1.38703984532215*src[7*src_stridea];
-        const float x09 = x00 + x04;
-        const float x0a = x01 + x03;
-        const float x0b = 1.4142135623731*x02;
-        const float x0c = x00 - x04;
-        const float x0d = x01 - x03;
-        const float x0e = 0.353553390593274 * (x09 - x0b);
-        const float x0f = 0.353553390593274 * (x0c + x0d);
-        const float x10 = 0.353553390593274 * (x0c - x0d);
-        const float x11 = 1.4142135623731*x06;
-        const float x12 = x05 + x07;
-        const float x13 = x05 - x07;
-        const float x14 = 0.353553390593274 * (x11 + x12);
-        const float x15 = 0.353553390593274 * (x11 - x12);
-        const float x16 = 0.5*x13;
-        const float x08 = -x15;
-        dst[0*dst_stridea] = 0.25 * (x09 + x0b) + 0.353553390593274*x0a;
-        dst[1*dst_stridea] = 0.707106781186547 * (x0f - x08);
-        dst[2*dst_stridea] = 0.707106781186547 * (x0f + x08);
-        dst[3*dst_stridea] = 0.707106781186547 * (x0e + x16);
-        dst[4*dst_stridea] = 0.707106781186547 * (x0e - x16);
-        dst[5*dst_stridea] = 0.707106781186547 * (x10 - x14);
-        dst[6*dst_stridea] = 0.707106781186547 * (x10 + x14);
-        dst[7*dst_stridea] = 0.25 * (x09 + x0b) - 0.353553390593274*x0a;
-        dst += dst_strideb;
-        src += src_strideb;
-    }
-}
-
-static void idct(float *dst, const float *src)
-{
-    float tmp[N*N];
-    idct_1d(tmp, src, 1, N, 1, N);
-    idct_1d(dst, tmp, N, 1, N, 1);
-}
 static float dct_matrix    [N*N];
 static float dct_trp_matrix[N*N];
 
@@ -199,15 +106,13 @@ static void idct_ref(float *dst, const float *src)
     dct_1d_ref(tmp, src, 1, N, dct_trp_matrix);
     dct_1d_ref(dst, tmp, N, 1, dct_trp_matrix);
 }
-
-
-int* filehand(char* filename) {
+void filehand(char* filename) {
  
     
   
-	int fd_read = cfs_open(filename, CFS_READ);
+	u8_t fd_read = cfs_open(filename, CFS_READ);
 	int i=0,j=0;
-	unsigned char info1[56],data[16384],rmng[2000];
+	unsigned char info1[56],data[LENGTH*LENGTH],rmng[2000];
 	cfs_read(fd_read,info1, 56); 										// read the 54-byte headr
 	int width1 = *(int*)&info1[18];
 	int height1 = *(int*)&info1[22];
@@ -220,24 +125,23 @@ int* filehand(char* filename) {
 	
 	
 	printf("Reading from file:\n");
-	for(i=0;i<16384;i++)
+	for(i=0;i<LENGTH*LENGTH;i++)
 	{
-		if(((i!=0)&&(i%128))==0)
+		if(((i!=0)&&(i%LENGTH))==0)
 		{
 			j++;
 			printf("\n");
 			
 		}
-		mat[j][i%128]=data[i];
-		printf("%d ",mat[j][i%128]);
+		mat[j][i%LENGTH]=data[i];
+		printf("%d ",mat[j][i%LENGTH]);
 	}
 
 	cfs_close(fd_read);
 	
-    return *mat;   
 }
 char rev[4];
-char* intochar(int num)
+void intochar(int num)
 {
 	char str[4];
 	
@@ -259,26 +163,24 @@ char* intochar(int num)
 	}
    rev[k]='\0';
    //printf("%s",rev);
-   return rev;
+   
 }
-void filewrite(char *OUT)
+void filewrite(char* out)
 {
 	int i,j,k,pix;
-	char ch1[4];
-	char ch2[4];
-	int fd_write = cfs_open(OUT, CFS_WRITE);
-	char* header="P2\n128 128\n255\n";
+	int fd_write = cfs_open(out, CFS_WRITE);
+	char* header="P2\n64 64\n255\n";	//change image size of the image here also
 	char* space=" ";
 	char* nline="\n";
 	
 	
 	cfs_write(fd_write,header,15);
 	printf("\n\n\nwriting\n");
-	for(i=0;i<128;i++)
+	for(i=0;i<LENGTH;i++)
 	{
-		for(j=0;j<128;j=j+1)
+		for(j=0;j<LENGTH;j=j+1)
 		{
-			*ch1=intochar(mat[i][j]);
+			intochar(mat[i][j]);
 			cfs_write(fd_write,rev,4);
 			//printf("%s ",rev);
 			cfs_write(fd_write,space,1);
@@ -300,24 +202,24 @@ PROCESS_THREAD(hello_world_process, ev, data)
 {
   PROCESS_BEGIN();
   printf("dct\n");
-   int i,j,k,l,p,in1[128][128],in2[128][128];
-    int* matrix1,*matrix2;
+   int i,j,k,l,p,in1[LENGTH][LENGTH],in2[LENGTH][LENGTH];
+    
     float src1[N*N],src2[N*N];
     float out_fdct1[N*N],out_fdct2[N*N], out_idct[N*N];
     
     
-	matrix1=filehand(INFILE1);
+	filehand(INFILE1);
 	for(i=0;i<LENGTH;i++)
 			for(j=0;j<LENGTH;j++)
 				in1[i][j]=mat[i][j];//store the returned matrix in in1
 
-	matrix2=filehand(INFILE2);
+	filehand(INFILE2);
 	for(i=0;i<LENGTH;i++)
 			for(j=0;j<LENGTH;j++)					
 				in2[i][j]=(mat[i][j]);//store the returned matrix in in2
 	
 	
-	init_dct();                        //dct computation starts
+
 	for(j=0;j<LENGTH/N;j++)
 	{
 	   for(i=0;i<LENGTH/N;i++)
@@ -328,16 +230,16 @@ PROCESS_THREAD(hello_world_process, ev, data)
 				{
 					src1[l+N*k]=in1[(N*j+k)][(l+N*i)];//store 8X8 image block into an array
 					src2[l+N*k]=in2[(N*j+k)][(l+N*i)];
-					printf("src:%f ",src1[l+N*k]);
+					//printf("src:%f ",src1[l+N*k]);
 				}
 				printf("\n");
 			}
 			printf("\n");
-			
+			init_dct();
 			fdct_ref(out_fdct1, src1);//find fast dct for each 8X8 blocks
 			fdct_ref(out_fdct2, src2);
 			int med;
-			for(med=0;med<64;med++)
+			for(med=0;med<N*N;med++)
 			{
 				out_fdct1[med]=(out_fdct1[med]+out_fdct2[med])/2;//picking the best pixel from 2 inputs
 			}
