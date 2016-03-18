5e41bb66173e646d9848be7bac2cbd970c0b26de
Dhyan Deep AK <dhyandeep7@gmail.com>
2016-03-18 15:08:01 +0530

    Added files via upload

---
+++ moteread.c
@@ -0,0 +1,32 @@
+#include "contiki.h"
+#include "/home/dhyandeepak/Desktop/contiki-2.7/cpu/msp430/dev/uart1.h"
+#include <stdio.h>
+#define SERIAL_BUF_SIZE 128
+static int uart_rx_callback(unsigned char c) {
+     uint8_t u;
+     printf("\nReceived %c",c);
+     u = (uint8_t)c;
+     printf("\nReceived %u",u);
+     
+     return u;
+     
+}
+PROCESS(moteread_process, "moteread process");
+static char rx_buf[SERIAL_BUF_SIZE];
+/*---------------------------------------------------------------------------*/
+
+AUTOSTART_PROCESSES(&moteread_process);
+
+/*---------------------------------------------------------------------------*/
+
+
+PROCESS_THREAD(moteread_process, ev, data)
+{
+	
+	PROCESS_BEGIN();
+	uart1_init(BAUD2UBR(115200)); //set the baud rate as necessary
+	uart1_set_input(uart_rx_callback); //set the callback function
+	PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+
---
+++ writeserial.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import serial
+length=8
+height=8
+ser = serial.Serial(
+   port='/dev/ttyUSB0',\
+   baudrate=115200,\
+   parity=serial.PARITY_NONE,\
+   stopbits=serial.STOPBITS_ONE,\
+   bytesize=serial.EIGHTBITS,\
+   timeout=0)
+print("connected to: " + ser.portstr)
+ser.write("help\n");
+
+f=open("read.txt","w");
+Matrix = [[0 for x in range(length)] for x in range(height)]
+for i in range(height):
+	for j in range(length):
+		ser.write(str(Matrix[i][j]))
+f.close();
