397c9805fd4974fc3b0dd3da1013ecb572950216
Dhyan Deep AK <dhyandeep7@gmail.com>
2016-03-18 12:34:16 +0530

    Read matrix, tokenize store into file

--- read.py
+++ read.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import serial
+length=8
+height=8
 ser = serial.Serial(
    port='/dev/ttyUSB0',\
    baudrate=115200,\
@@ -11,16 +13,32 @@ print("connected to: " + ser.portstr)
 ser.write("help\n");
 
 f=open("read.txt","w");
+Matrix = [[0 for x in range(length)] for x in range(height)] 
+
 data=0
 while data <> "<":
 	data = ser.read();
 text=""
-while data<>'>':
+while data<>">":
 	data = ser.read();
+	if data==">":
+		break
 	if data:
 		print(data),
 		f.write(data)
 		text=text+data;
+	
 values=text.split()
-print (values)
+i=0
+while i < len(values):
+	print values[i]
+	i=i+1
+p=0
+for i in range(0,height,8):
+	for j in range(0,length,8):
+		for k in range(0,8):
+			for l in range(0,8):
+				Matrix[i+k][j+l]=values[p]
+				p=p+1
+print(Matrix)
 f.close();
