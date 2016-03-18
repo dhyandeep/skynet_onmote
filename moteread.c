9a1b247a0507c0d9ae0b0295e5ce2ed7add6f7c3
Dhyan Deep AK <dhyandeep7@gmail.com>
2016-03-19 01:26:59 +0530

    Added files via upload
    read charecters converted into integer values of pixels

--- moteread.c
+++ moteread.c
@@ -2,14 +2,24 @@
 #include "/home/dhyandeepak/Desktop/contiki-2.7/cpu/msp430/dev/uart1.h"
 #include <stdio.h>
 #define SERIAL_BUF_SIZE 128
+int currentnum=0,val=0;
 static int uart_rx_callback(unsigned char c) {
      uint8_t u;
      printf("\nReceived %c",c);
      u = (uint8_t)c;
-     printf("\nReceived %u",u);
-     
+     //printf("\nReceived %u",u);
+     if(c!=' ')
+     {
+		 val=c-'0';
+		 currentnum=currentnum*10+val;
+	 }
+	 else
+	 {
+		 printf("\nvalue:%d",currentnum);
+		 val=0;
+		 currentnum=0;
+	 }
      return u;
-     
 }
 PROCESS(moteread_process, "moteread process");
 static char rx_buf[SERIAL_BUF_SIZE];
--- writeserial.py
+++ writeserial.py
@@ -16,5 +16,7 @@ f=open("read.txt","w");
 Matrix = [[0 for x in range(length)] for x in range(height)]
 for i in range(height):
 	for j in range(length):
+		Matrix[i][j]=(i+j)%20
 		ser.write(str(Matrix[i][j]))
+		ser.write(" ")
 f.close();
